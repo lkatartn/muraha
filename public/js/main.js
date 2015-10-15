600e35ab3f4d1748f5f1a5f89f7e58e1124e4b32
Alexey Kutas <lkatartn@gmail.com>
2015-10-15 16:44:38 +0300

    better modularity

--- public/js/main.js
+++ public/js/main.js
@@ -17,7 +17,10 @@ requirejs.config({
 
 
 require(['Muraha'], function(Muraha) {
-		var muraha = new Muraha(200, 150);
-		
-		
+		var muraha = new Muraha(100, 80);
+		muraha.addAnt(10,10);
+		muraha.addAnt(20,10);
+		muraha.addAnt(30,10);
+		muraha.addAnt(10,10);
+		muraha.start();		
 })
--- public/js/muraha.js
+++ public/js/muraha.js
@@ -1,28 +1,24 @@
 define('Muraha', ['Anthill','Ant','lodash'], function(Anthill, Ant, _){
 	var Muraha = function(anthillWidth, anthillHeight){
-		var ctx = document.getElementById('muraha-anthill').getContext('2d');
+		//setting context for global functions
+		var muraha=this;
+
+		//setting own params
+		this.ctx = document.getElementById('muraha-anthill').getContext('2d');
 	    this.anthill = new Anthill(anthillWidth, anthillHeight)
 	    this.ants=[];
-		anthill.draw(ctx);
 		this.speed = 1;
-		for (var i= 0; i< 10; i++) {
+		this.timer=null;
+		//setting own functions
+		this.addAnt = function(posx,posy) {
 			this.ants.push(new Ant({
-				x: 2*i,
-				y: 2*i,
-				fingerprint: i+1
+				x: posx,
+				y: posy,
+				fingerprint:muraha.ants.length+1
 			}))
 		}
-
-		
-		function drawLoop() {
-			anthill.draw(ctx);
-			window.requestAnimationFrame(drawLoop)
-		}
-		
-		window.requestAnimationFrame(drawLoop);
 		this.start = function(){
-			var muraha=this;
-			this.timer = setInterval(function(){
+			muraha.timer = setInterval(function(){
 				_.times(muraha.speed, function(){
 					for(ant in muraha.ants) {
 						muraha.ants[ant].rule(muraha.anthill.field)
@@ -30,18 +26,26 @@ define('Muraha', ['Anthill','Ant','lodash'], function(Anthill, Ant, _){
 				})
 			}, 30);
 		};
-		muraha.stop = function() {
-			clearInterval(this.timer);
+		this.stop = function() {
+			clearInterval(muraha.timer);
 		}
-		
-		//buttons listeners
-		document.getElementById('muraha-start').onclick=muraha.start;
-		document.getElementById('muraha-stop').onclick=muraha.stop;
+		this.drawLoop= function() {
+			muraha.anthill.draw(muraha.ctx);
+			window.requestAnimationFrame(muraha.drawLoop)
+		}
+
+		//setting controls
+		window.requestAnimationFrame(this.drawLoop);
+		document.getElementById('muraha-start').onclick=this.start;
+		document.getElementById('muraha-stop').onclick=this.stop;
 		document.getElementById('muraha-set-speed').onclick = function() {
-			muraha.speed = Math.floor(prompt('Enter the speed >1',1));
+			muraha.speed = Math.floor(prompt('Enter the speed >=1',1));
 			muraha.stop();
 			muraha.start();
 		}
+		//draw the field
+		this.anthill.draw(this.ctx);
+		
 	}
 	    
 	return Muraha;	
