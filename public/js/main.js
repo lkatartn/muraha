7af5daaa728da19161498d6440d58a2d89431507
Alexey Kutas <lkatartn@gmail.com>
2015-10-15 14:40:05 +0300

    added fingerprint and field borders collision checking

--- public/js/main.js
+++ public/js/main.js
@@ -21,21 +21,32 @@ require(['Anthill', 'Ant', 'lodash'], function(Anthill, Ant, _ ) {
 	    var anthill = new Anthill(120,80)
 		anthill.draw(ctx);
 
-		var ant = new Ant({
-			x: 50,
-			y: 40,
-			direction: 'w'
-		})
-		setInterval(function(){
-			_.times(3, function(){
-				ant.rule(anthill.field)
-			})
-		}, 0);
+		for (var i= 0; i< 10; i++) {
+			anthill.ants.push(new Ant({
+				x: 2*i,
+				y: 2*i,
+				fingerprint: i+1
+			}))
+		}
+
+		
 		function drawLoop() {
 			anthill.draw(ctx);
 			window.requestAnimationFrame(drawLoop)
 		}
 		window.requestAnimationFrame(drawLoop);
-
+		window.start = function(speed){
+			return setInterval(function(){
+				_.times(speed, function(){
+					for(ant in anthill.ants) {
+						anthill.ants[ant].rule(anthill.field)
+					}
+				})
+			}, 0);
+		};
+		window.stop = function(timerId) {
+			clearInterval(timerId)
+		}
+		window.timerId = window.start(1);
 		
 })
